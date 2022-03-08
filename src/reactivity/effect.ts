ab44f8537c6c02051c767648d7871f6a1b9950d5
An0510 <827479225@qq.com>
2022-03-08 21:18:01 +0800

    reactive基础依赖收集和触发

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,50 @@
+// 全局,用于获取依赖
+const targetMap = new Map()
+// 为了将run方法中的effect中fn传递给track函数
+let activeEffect = void 0
+//? 依赖类
+class ReactiveEffect{
+    constructor(public fn) {
+
+    }
+    run(){
+        activeEffect = this
+        this.fn()
+    }
+}
+
+export function effect(fn){
+    // fn
+    const _effect = new ReactiveEffect(fn)
+
+    _effect.run()
+
+}
+
+//? 收集依赖
+export function track(target,key){
+    // target -> key -> dep
+    let depsMap = targetMap.get(target)
+    if (!depsMap){
+        depsMap = new Map()
+        targetMap.set(target,depsMap)
+    }
+    let dep = depsMap.get(key)
+    // 如果没有dep 新建dep并更新depsMap
+    if(!dep){
+        dep = new Set()
+        depsMap.set(key,dep)
+    }
+    // 依赖实际上就收集到了targetMap中
+    dep.add(activeEffect)
+}
+//? 触发依赖 更新依赖
+export function trigger(target, key){
+    // 从全局变量depsMap中拿到对应target对应key的
+    let depsMap = targetMap.get(target)
+    let dep = depsMap.get(key)
+    // 将所有依赖遍历执行
+    for (const effect of dep) {
+        effect.run()
+    }
+}
--- src/reactivity/test/effect.spec.ts
+++ src/reactivity/test/effect.spec.ts
@@ -1,5 +1,16 @@
+import {reactive} from "../reactive";
+import {effect} from "../effect";
 describe('effect',() => {
-    it('happy path',() => {
 
+    it("happy path",() => {
+        let dummy;
+        const counter = reactive({ num: 0 });
+        effect(() => (dummy = counter.num));
+
+        expect(dummy).toBe(0);
+        // Todo
+        counter.num = 7;
+        expect(dummy).toBe(7);
     })
+
 })
